9bba3f3ae9654a83ade6802e0c4c4bdbf484ecda
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-01 12:59:42 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,12 @@
 class Owner
   attr_accessor :owner
+  attr_reader :species
   @@all = []
 
   def initialize(owner)
     @owner = owner
     @@all << self
+    @species = species
   end
   def self.all
     @@all
@@ -16,5 +18,9 @@ class Owner
 
   def self.count
     @@all.count
+  end
+
+  def say_species
+    @species
   end
 end
